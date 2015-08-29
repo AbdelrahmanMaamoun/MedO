b1c995a6c6c6ecaad27fefd1f5755c1f5a1f3474
AbdelrahmanMaamoun <abdelrahman.maamoun@gmail.com>
2015-08-29 08:57:43 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MedO::Application.config.secret_token = 'a1beb2a0854dc072c8f4335a657e16fdd25f24701362f70f2084ab9ec3ec08d7b99ac30075f3c33e247fdb195ff1b476032935162f0387b636fb55b238fbd891'
